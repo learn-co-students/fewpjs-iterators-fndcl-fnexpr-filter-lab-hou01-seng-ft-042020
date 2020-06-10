58d7d56ac78ff9b4bfe051f4755f651559663060
Stephanie Cheney <steph@scheneyphotography.com>
2020-06-10 15:24:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(driver => {return driver.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+
+function matchName(drivers, name){
+    return drivers.filter(driver=>{return driver.name===name})
+}
