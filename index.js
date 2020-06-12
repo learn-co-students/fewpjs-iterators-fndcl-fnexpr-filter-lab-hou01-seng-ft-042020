1be86a0f861797f85ede8037a383e3c6fab74bce
estherkimyunjung <estherkimyunjung@gmail.com>
2020-06-11 22:47:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(dirvers, name){
+  return dirvers.filter(element => element.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(dirvers, letter){
+  return dirvers.filter(element => element.toLowerCase()[0] === letter.toLowerCase()[0])
+}
+
+function matchName(dirvers, findName){
+  return dirvers.filter(element => element.name === findName)
+}
